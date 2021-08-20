4ad03c3fd0fa1e7229e1088a519aef393e988544
Nonny177 <55391624+Nonny177@users.noreply.github.com>
2021-08-20 08:25:44 +0200

    Created a fizz_buzz method

--- index.py
+++ index.py
@@ -10,3 +10,13 @@ def doubler(numbers):
     for i in range(len(numbers)):
         multiply.append(numbers[i]*2)
     print(multiply)
+
+def fizz_buzz(max):
+    result = []
+    for i in range(max):
+        if(i != 0):
+            if(i % 4 == 0 or i % 6 == 0):
+                if(i % 4 == 0 and i % 6 == 0):
+                    continue   
+                result.append(i)
+    print(result)    
